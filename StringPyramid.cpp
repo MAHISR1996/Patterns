88c40f60170484f17974e12df638eddc805f9766
MAHISR1996 <42845640+MAHISR1996@users.noreply.github.com>
2018-08-30 23:04:36 +0530

    Another way to build pyramid

--- StringPyramid.cpp
+++ StringPyramid.cpp
@@ -3,32 +3,22 @@
 using namespace std;
 int main()
 {
-    int i,j,k=0,len=0,flag=0,l,y,z;
+    int i,j,k=0,n=0,flag=0,l,y,z;
     char ch[20];
     cout<<"enter the name\n";
     cin.getline(ch,15);
-    len=strlen(ch);
-    for(i=0;i<len;i++)
-    {
-        for(j=0;j<=i;j++)
-        {
-       
-            if(ch[k]!='\0')
-            cout<<ch[k]<<" ";
-          if(ch[k+1]=='\0')
-          { y=j;
-            z=i;
-              goto a;
-          }
-          else 
-          k++;
-        }
-        cout<<"\n";
-    }
-    a:while(y!=z)
-    {
-        cout<<"#"<<" ";
-        y++;
+    n=strlen(ch);
+     for(i=0;k<n;i++)
+     {
+         for(j=0;j<=i;j++)
+         {
+         if(k<=n-1)
+         cout<<ch[k];
+         else
+        cout<<"#";
+         k++;
+         }
+         cout<<endl;
     }
     return 0;
 }
